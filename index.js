72927c67cb5ed54bd60464e7ba316dad7baf7704
Sakar Masoud <masoudsakar@gmail.com>
2022-10-12 14:17:31 +0300

    Sakar's Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+let currentUser="Sakar";
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+let shortGreeting = `Welcome, ${firstInitial}!`;
